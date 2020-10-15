8977c5e9aead9db8f2848a69f6b6973561267c1a
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-10-15 12:15:22 -0400

    V2

--- src/Tgstation.Server.DMApiUpdater/Program.cs
+++ src/Tgstation.Server.DMApiUpdater/Program.cs
@@ -1,5 +1,4 @@
-﻿using LibGit2Sharp;
-using Octokit;
+﻿using Octokit;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -44,8 +43,9 @@ namespace Tgstation.Server.DMApiUpdater
 		{
 			var headerPath = args[0];
 			var libraryPath = args[1];
-			var targetBranch = args[2];
-			var gitHubToken = args[3];
+			var gitHubToken = args[2];
+			var repoPath = args[3];
+
 			var repoSlug = Environment.GetEnvironmentVariable("GITHUB_REPOSITORY");
 			var repoSplits = repoSlug.Split('/');
 			var repoOwner = repoSplits[0];
@@ -58,39 +58,13 @@ namespace Tgstation.Server.DMApiUpdater
 
 			var releaseTask = GetLatestDMApiBytes(gitHubClient, repoOwner, repoName);
 
-			var repoUrl = $"https://github.com/{repoOwner}/{repoName}";
-			Console.WriteLine($"Cloning {repoUrl}...");
-
-			const string BaseRepoPath = "./cloned_repo";
-			LibGit2Sharp.Repository.Clone(repoUrl, BaseRepoPath, new CloneOptions
-			{
-				BranchName = targetBranch,
-				Checkout = true,
-				RecurseSubmodules = false,
-				CredentialsProvider = (url, usernameFromUrl, supportedCredentialTypes) =>
-				{
-					return new UsernamePasswordCredentials
-					{
-						Username = gitHubToken,
-						Password = "x-oauth-basic"
-					};
-				}
-			});
-
-			Console.WriteLine("Loading repository...");
-			using var repo = new LibGit2Sharp.Repository(BaseRepoPath);
-
-			const string BranchName = "tgs-dmapi-updater-temp-branch";
-			var workingBranch = repo.CreateBranch(BranchName);
-			Commands.Checkout(repo, workingBranch);
-
 			Console.WriteLine("Deleting old DMAPI...");
 
-			var targetHeaderPath = Path.Combine(BaseRepoPath, headerPath);
+			var targetHeaderPath = Path.Combine(repoPath, headerPath);
 			if (File.Exists(targetHeaderPath))
 				File.Delete(targetHeaderPath);
 
-			var targetLibraryPath = Path.Combine(BaseRepoPath, libraryPath);
+			var targetLibraryPath = Path.Combine(repoPath, libraryPath);
 			if (Directory.Exists(targetLibraryPath))
 				RecursiveDelete(targetLibraryPath);
 
@@ -114,56 +88,6 @@ namespace Tgstation.Server.DMApiUpdater
 				var dir = Path.GetDirectoryName(targetPath);
 				Directory.CreateDirectory(dir);
 				entry.ExtractToFile(targetPath);
-			}
-
-			var status = repo.RetrieveStatus();
-			if (!status.IsDirty)
-			{
-				Console.WriteLine("Repository is clean, nothing to do!");
-				return;
-			}
-
-			Console.WriteLine("Staging files...");
-			Commands.Stage(repo, "*");
-
-			Console.WriteLine("Creating commit...");
-			var signature = new LibGit2Sharp.Signature(
-				new Identity(
-					"tgstation-server",
-					"tgstation-server@users.noreply.github.com"),
-				DateTimeOffset.Now);
-			repo.Commit($"Update to TGS {releaseTuple.Item2}", signature, signature);
-
-			Console.WriteLine($"Force pushing to origin/{BranchName}...");
-			var remote = repo.Network.Remotes.First();
-			repo.Network.Push(remote, $"+refs/heads/{BranchName}", new PushOptions
-			{
-				CredentialsProvider = (url, usernameFromUrl, supportedCredentialTypes) =>
-				{
-					return new UsernamePasswordCredentials
-					{
-						Username = gitHubToken,
-						Password = "x-oauth-basic"
-					};
-				}
-			});
-
-			try
-			{
-				Console.WriteLine($"Creating pull request...");
-				await gitHubClient.PullRequest.Create(
-					repoOwner,
-					repoName,
-					new NewPullRequest("Update TGS DMAPI", BranchName, targetBranch)
-					{
-						Body = "This PR automatically updates the TGS DMAPI to the latest version. Please note that major changes need further modifications. Features added in minor updates may also require further implementation.",
-						Draft = false,
-						MaintainerCanModify = true
-					});
-			}
-			catch (Exception ex) when (ex is ApiValidationException apiValidationException && apiValidationException.StatusCode == HttpStatusCode.UnprocessableEntity)
-			{
-				Console.WriteLine("Unable to create PR, it seems to already exist. It should be updated now.");
 			}
 		}
 	}
